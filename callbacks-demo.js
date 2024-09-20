f958ec4f2595235bf80153e7c60d31fbd125c199
ravitmg <tmgrawv@gmail.com>
2024-09-20 17:48:31 +0545

    sepot 20 code

--- callbacks-demo.js
+++ callbacks-demo.js
@@ -1,96 +1,81 @@
 
-function fetchFromApiCB(successFunction) {
-    // usually a call is made to the server
-    setTimeout(() => {
-        successFunction("some data in callback");
-    }, 1);
-}
+// function fetchFromApiCB(successFunction) {
+//     // usually a call is made to the server
+//     setTimeout(() => {
+//         successFunction("some data in callback");
+//     }, 1);
+// }
 
-function fetchFromAPI() {
-    return "some data";
-}
+// function fetchFromAPI() {
+//     return "some data";
+// }
 
-const data1 = fetchFromAPI();
-console.log(data1);
+// const data1 = fetchFromAPI();
+// console.log(data1);
 
-fetchFromApiCB((data) => {
-    console.log(data);
-});
+// fetchFromApiCB((data) => {
+//     console.log(data);
+// });
 
-console.log("done");
+// console.log("done");
 
-function customForEach(array, callback) {
-    for (let i = 0; i < array.length; i++) {
-        callback(array[i], i);
-    }
-}
+// function customForEach(array, callback) {
+//     for (let i = 0; i < array.length; i++) {
+//         callback(array[i], i);
+//     }
+// }
 
-const myArray = [
-    "ram",
-    "sita",
-    "ravan",
-    "laxman",
-    "laxman",
-    "laxman",
-    "laxman",
-    "laxman",
-    "laxman",
-    "laxman",
-    "laxman",
-];
+// const myArray = [
+//     "ram",
+//     "sita",
+//     "ravan",
+//     "laxman",
+//     "laxman",
+//     "laxman",
+//     "laxman",
+//     "laxman",
+//     "laxman",
+//     "laxman",
+//     "laxman",
+// ];
 
-customForEach(myArray, (item, index) => {
-    console.log(item, index);
-});
+// customForEach(myArray, (item, index) => {
+//     console.log(item, index);
+// });
 
-console.log("bahira")
+// console.log("bahira")
 
-setTimeout(() => {
-    console.log("timeout vitra ko code")
-});
+// setTimeout(() => {
+//     console.log("timeout vitra ko code")
+// });
 
-console.log("don");
+// console.log("don");
 
 
 function fetchFromApiPromise() {
     return new Promise((resol, reject) => {
         setTimeout(() => {
-            resol("some data")
+            reject("some data")
         }, 1)
     })
 }
 
-const promise = fetchFromApiPromise()
+fetchFromApiPromise()
     .then((data) => {
-
         console.log(data);
-        return "modified promise data"
-    })
-    .then((data2) => {
-        console.log(data2);
-    })
-    .catch((e) => {
-        console.log("error block")
+    });
+
+async function fetchAndDisplayData () {
+    try {
+        const x = y + z;
+        const newData = await fetchFromApiPromise();
+        
+        console.log("await pachi");
+        console.log(newData);
+    } catch (e) {
+        console.log("error aayo");
         console.log(e);
-    })
-
-
-function fetchFromApi2Promise() {
-    return new Promise((resol, reject) => {
-        setTimeout(() => {
-            resol("some data 2")
-        }, 1)
-    })
+    }
 }
 
-Promise.all([
-    fetchFromApiPromise(),
-    fetchFromApi2Promise(),
-]).then((data) => {
-    console.log(data)
-    return data[0]
-}).then ((data2) => {
-    console.log(data2)
-}).catch(e => {
-    console.log(e);
-})
+fetchAndDisplayData()
--- src/pages/posts.js
+++ src/pages/posts.js
@@ -1,30 +1,40 @@
 import { useEffect, useState } from "react";
-import Post from "./post-item";
 import PostItem from "./post-item";
+import { AddPost } from "./add-post";
 
 export default function Posts() {
     const [posts, setPosts] = useState([]);
 
     useEffect(() => {
-        fetch("https://jsonplaceholder.typicode.com/posts")
-            .then(response => response.json())
-            .then((data) => {
-                console.log(data);
-                setPosts(data);
-            })
-            .catch(e => {
-                alert("Some error occured");
-            })
-            .finally(() => {
-                console.log("data fetched. It either succeeded or failed");
-            });
+        async function fetchData() {
+            const response = await fetch("https://jsonplaceholder.typicode.com/posts")
+            const data = await response.json();
+            console.log(data);
+            setPosts(data);
+        }
+        fetchData();
     }, []);
+
+    const handleSubmit = async (postData) => {
+        const response = await fetch('https://jsonplaceholder.typicode.com/posts', {
+            method: 'POST',
+            body: JSON.stringify(postData),
+            headers: {
+                'Content-type': 'application/json; charset=UTF-8',
+            },
+        });
+        const data = await response.json();
+        console.log(data);
+    };
 
     return (
         <div>
             <h1>
                 Today's Posts
             </h1>
+            <AddPost
+                onSubmit={handleSubmit}
+            />
             <div>
                 {
                     posts.map((post, index) => {
