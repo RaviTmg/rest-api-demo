525a7ca60242f5789de0d2ce75d961fd967b5394
ravitmg <tmgrawv@gmail.com>
2024-09-25 16:50:03 +0545

    sept 25

--- src/pages/add-post.js
+++ src/pages/add-post.js
@@ -2,7 +2,7 @@ import { useState } from "react";
 import TextInput from "./components/text-input";
 
 
-export function AddPost({ onSubmit }) {
+export default function AddPost({ onSubmit }) {
     const [title, setTitle] = useState("");
     const [body, setBody] = useState("");
     const [userId, setUserId] = useState();
@@ -16,26 +16,32 @@ export function AddPost({ onSubmit }) {
         });
     }
     return (
-        <form 
+        <form
             onSubmit={handleSubmit}
-            className="flex flex-col items-start"
+            className="flex flex-col items-start gap-5 rounded p-3 border border-gray-500"
         >
             <label>
-                Title:
+                <div>
+                    Title:
+                </div>
                 <TextInput
                     value={title}
                     onChange={e => setTitle(e.target.value)}
                 />
             </label>
             <label>
-                Body:
+                <div>
+                    Body:
+                </div>
                 <TextInput
                     value={body}
                     onChange={e => setBody(e.target.value)}
                 />
             </label>
             <label>
-                User Id:
+                <div className="mr-2">
+                    User Id:
+                </div>
                 <TextInput
                     type="number"
                     value={userId}
